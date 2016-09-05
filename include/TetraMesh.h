2f766dab3d50832783a64f7886f5a7c7f4af601f
Petros Kataras <petroskataras@gmail.com>
2016-09-05 14:49:12 +0200

    Start seperating rendering from tetrahedralization.

--- include/TetraMesh.h
+++ include/TetraMesh.h
@@ -9,30 +9,21 @@
 #define TetraMesh_hpp
 
 #include "cinder/Cinder.h"
-#include "cinder/gl/gl.h"
 
 #include "TetrahedronTopology.h"
 #include "TriMeshLoader.h"
-#include "Octree.h"
 
 #include "CGALTetrahedralize.h"
 #include "CGALUtils.h"
+#include "cinder/Utilities.h"
 
 namespace tetra {
 
-struct DrawArraysIndirectCommand{
-    GLuint vertexCount;
-    GLuint instanceCount;
-    GLuint firstVertex;
-    GLuint baseInstance;
-};
-  
 using namespace ci;
 
 using TetraTopologyRef = std::shared_ptr<TetraTools::TetrahedronTopology>;
 using TriangleTopologyRef = std::shared_ptr<TetraTools::TriangleTopology>;
 using CGALTetrahedralizeRef = std::shared_ptr<CGALTetrahedralize>;
-using TetraBatchRefVec = std::vector<gl::BatchRef>;
 using TetraMeshRef = std::shared_ptr<class TetraMesh>;
 
 class TetraMesh {
@@ -53,15 +44,8 @@ class TetraMesh {
                     const double cellRadiusEdgeRatio );
     
         void update();
-        void draw();
-        
-        void multiDrawIndirect();
         const TetraTopologyRef& getTopology() const { return mTopology; }
-        const gl::BatchRef& getMDIBatch() const { return mTetraMDI; }
-        const gl::GlslProgRef& getMDIGlsl() const { return mGlslMDI; }
     private:
-        void generateTetraBatches( const TetraTopologyRef &tetraTopology );
-    
         TriangleTopologyRef loadSurface( const std::string &filename );
     
         TetraTopologyRef generateTetrasFromSurface( const TriangleTopologyRef &triMesh,
@@ -72,15 +56,9 @@ class TetraMesh {
                                                     const double cellRadiusEdgeRatio );
         TetraTopologyRef mTopology;
         TriangleTopologyRef mSurface;
-        TetraBatchRefVec mTetraBatches;
-        gl::BatchRef mTetraMDI;
         std::vector<vec3> mVertices;
         std::vector<vec3> mNormals;
         std::vector<vec3> mCentroids;
-        std::vector<mat4> mModelMatrices;
-        gl::GlslProgRef mGlslMDI;
-        gl::VboRef mIndirectBuffer;
-        gl::VboRef mModelMatricesVbo;
 };
 
 } // namespace Tetra
--- src/TetraMesh.cpp
+++ src/TetraMesh.cpp
@@ -21,26 +21,7 @@ TetraMesh::TetraMesh( const fs::path& path,
                         const double facetDistance,
                         const double cellRadiusEdgeRatio )
 {
-    generateTetraBatches( generateTetrasFromSurface( loadSurface( path.string() ), cellSize, facetAngle, facetSize, facetDistance, cellRadiusEdgeRatio ) );
-}
-
-void TetraMesh::draw()
-{
-    for( const auto& batch : mTetraBatches ) {
-        if( batch ) 
-            batch->draw();
-    }
-}
-
-void TetraMesh::multiDrawIndirect()
-{
-    if( !mTopology || mTopology->GetTetrahedra().size() == 0 ) return;
-
-    gl::ScopedGlslProg ScopedGlslProg( mTetraMDI->getGlslProg() );
-    auto vao = mTetraMDI->getVao();
-    gl::ScopedVao ScopedVao( vao );
-    gl::setDefaultShaderVars();
-    glMultiDrawArraysIndirect( GL_TRIANGLES, NULL, mTopology->GetTetrahedra().size(), 0 );
+    generateTetrasFromSurface( loadSurface( path.string() ), cellSize, facetAngle, facetSize, facetDistance, cellRadiusEdgeRatio );
 }
 
 TriangleTopologyRef TetraMesh::loadSurface(const std::string& filename )
@@ -92,113 +73,6 @@ TetraTopologyRef TetraMesh::generateTetrasFromSurface( const TriangleTopologyRef
     mTopology->Init( cth->GetTetraVertices(), cth->GetTetras(), false );
     CI_LOG_I( "Generated tetrahedral mesh in : " << timer.getSeconds() << " with " << cth->GetTetras().size() << " tetras and " << cth->GetTetraVertices().size() << " vertices " );
     return mTopology;
-}
-
-
-
-void TetraMesh::generateTetraBatches( const TetraTopologyRef& tetraTopology )
-{
-    if( !tetraTopology ) return;
-    
-    mVertices.clear();
-    mNormals.clear();
-    mCentroids.clear();
-
-    const auto& tetrahedra = tetraTopology->GetTetrahedra();
-    const auto& verts = tetraTopology->GetVertices();
-    
-    for( const auto& tetra : tetrahedra ) {
-        
-        std::vector<uint32_t> indices;
-        std::vector<vec3> vertices;
-        std::vector<vec3> normals;
-    
-        Vec3f c = tetra.Centroid( verts[tetra.index[0]], verts[tetra.index[1]], verts[tetra.index[2]], verts[tetra.index[3]] );
-        
-        vec3 centroid = vec3( c.x, c.y, c.z );
-        mCentroids.push_back( centroid );
-        //CI_LOG_I( "Centroid : " << centroid );
-        vec3 v0 = vec3( verts[tetra.index[0]].x, verts[tetra.index[0]].y, verts[tetra.index[0]].z );
-        vec3 v1 = vec3( verts[tetra.index[1]].x, verts[tetra.index[1]].y, verts[tetra.index[1]].z );
-        vec3 v2 = vec3( verts[tetra.index[2]].x, verts[tetra.index[2]].y, verts[tetra.index[2]].z );
-        vec3 v3 = vec3( verts[tetra.index[3]].x, verts[tetra.index[3]].y, verts[tetra.index[3]].z );
-        
-        vec3 vc0 = ( v0 - centroid );// * .90f;
-        vec3 vc1 = ( v1 - centroid );// * .90f;
-        vec3 vc2 = ( v2 - centroid );// * .90f;
-        vec3 vc3 = ( v3 - centroid );// * .90f;
-        
-        vertices.push_back( centroid + vc0 );
-        vertices.push_back( centroid + vc3 );
-        vertices.push_back( centroid + vc2 );
-        
-        vertices.push_back( centroid + vc3 );
-        vertices.push_back( centroid + vc1 );
-        vertices.push_back( centroid + vc2 );
-        
-        vertices.push_back( centroid + vc1 );
-        vertices.push_back( centroid + vc0 );
-        vertices.push_back( centroid + vc2 );
-        
-        vertices.push_back( centroid + vc0 );
-        vertices.push_back( centroid + vc1 );
-        vertices.push_back( centroid + vc3 );
-
-        CGALUtils::calculateAddTetraNormals( vertices, normals );
-    
-        std::vector<gl::VboMesh::Layout> bufferLayout {
-            gl::VboMesh::Layout().usage( GL_STATIC_DRAW ).attrib( geom::Attrib::POSITION, 3 ),
-            gl::VboMesh::Layout().usage( GL_STATIC_DRAW ).attrib( geom::Attrib::NORMAL, 3 )
-        };
-        //gl::VboMeshRef vboMesh = gl::VboMesh::create( vertices.size(), GL_TRIANGLES, bufferLayout, 0, GL_UNSIGNED_INT, 0 );
-        //vboMesh->bufferAttrib( geom::POSITION, vertices.size() * sizeof( vec3 ), vertices.data() );
-        //vboMesh->bufferAttrib( geom::NORMAL, normals.size() * sizeof( vec3 ), normals.data() );
-    
-        //mTetraBatches.push_back( gl::Batch::create( vboMesh, gl::getStockShader( gl::ShaderDef().lambert() ) ) );
-
-        for( const auto& vertex : vertices ) {
-            mVertices.push_back( vertex );
-        }
-        for( const auto& norm : normals ) {
-            mNormals.push_back( norm );
-        }
-    }
-
-    auto centroidsVbo = gl::Vbo::create( GL_ARRAY_BUFFER, mCentroids.size() * sizeof( vec3 ), mCentroids.data(), GL_STATIC_DRAW );
-    geom::BufferLayout instanceCentroidsLayout;
-    instanceCentroidsLayout.append( geom::Attrib::CUSTOM_0, 3, 0, 0, 1 );
-
-    mModelMatrices.clear();
-    mModelMatrices.resize( tetrahedra.size() );
-    mModelMatricesVbo = gl::Vbo::create( GL_ARRAY_BUFFER, mModelMatrices.size() * sizeof( mat4 ), mModelMatrices.data(), GL_DYNAMIC_DRAW );
-    geom::BufferLayout instanceModelMatricesLayout;
-    instanceModelMatricesLayout.append( geom::Attrib::CUSTOM_1, 16, sizeof(mat4), 0, 1 );
-    //centroidsVbo->bufferData( GL_ARRAY_BUFFER, mCentroids.data(), GL_STATIC_DRAW );
-
-    mIndirectBuffer = gl::Vbo::create( GL_DRAW_INDIRECT_BUFFER );
-    mIndirectBuffer->bufferData( tetrahedra.size() * sizeof( DrawArraysIndirectCommand ), NULL, GL_STATIC_DRAW );
-    DrawArraysIndirectCommand * drawCmd = ( DrawArraysIndirectCommand* )mIndirectBuffer->mapBufferRange( 0, tetrahedra.size() * sizeof( DrawArraysIndirectCommand ), GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT );
-    for( auto i = 0; i < tetrahedra.size(); ++i ) {
-        drawCmd[i].vertexCount = 12;
-        drawCmd[i].instanceCount = 1;
-        drawCmd[i].firstVertex = i*12;
-        drawCmd[i].baseInstance = i;
-    }
-    mIndirectBuffer->unmap();
-
-    std::vector<gl::VboMesh::Layout> vertLayout {
-        gl::VboMesh::Layout().usage( GL_STATIC_DRAW ).attrib( geom::Attrib::POSITION, 3 ),
-        gl::VboMesh::Layout().usage( GL_STATIC_DRAW ).attrib( geom::Attrib::NORMAL, 3 )
-    };
-    auto indirectVboMesh = gl::VboMesh::create( mVertices.size(), GL_TRIANGLES, vertLayout );
-    indirectVboMesh->bufferAttrib( geom::Attrib::POSITION, mVertices );
-    indirectVboMesh->bufferAttrib( geom::Attrib::NORMAL, mNormals );
-
-    indirectVboMesh->appendVbo( instanceCentroidsLayout, centroidsVbo );
-    indirectVboMesh->appendVbo( instanceModelMatricesLayout, mModelMatricesVbo );
-    //mGlslMDI = gl::getStockShader( gl::ShaderDef().lambert().color() );
-    mGlslMDI = gl::GlslProg::create( gl::GlslProg::Format().vertex( loadAsset( "InstancedModelMatrix.vert" )).geometry( loadAsset("InstancedModelMatrix.geom") ).fragment( loadAsset( "InstancedModelMatrix.frag" ) ) );
-    mTetraMDI = gl::Batch::create( indirectVboMesh, mGlslMDI, { { geom::Attrib::CUSTOM_0, "centroidPosition" }, { geom::Attrib::CUSTOM_1, "aModelMatrix" } } );
 }
 
 void TetraMesh::update()
